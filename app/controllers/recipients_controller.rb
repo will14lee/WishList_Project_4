ca93405da03e552f86d144361236d789651dcde5
will14lee <will14lee@gmail.com>
2021-12-15 10:59:13 -0500

    Add recipient with full CRUD capabilities

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,2 +1,37 @@
 class RecipientsController < ApplicationController
+    def index
+        recipients=Recipient.all
+        render json: recipients
+    end
+    
+    def show
+        recipient= this_recipient
+        render json: recipient
+    end
+    
+    def update
+        recipient= this_recipient
+        recipient.update(recipient_params)
+        render json: recipient
+    end
+    
+    def create
+        recipient=Recipient.create(recipient_params)
+        render json: recipient, status: :created
+    end
+    
+    def destroy
+        recipient=this_recipient
+        recipient.destroy
+        head :no_content
+    end
+
+    private
+
+    def this_recipient
+        Recipient.find_by(params[id: params[:id]])
+    end
+    def recipient_params
+        params.permit(:user_id, :name, :age, :relationship, :image_url)
+    end
 end
