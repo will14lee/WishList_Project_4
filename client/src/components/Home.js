1a6c71fed5aa0677ca736362591932b17fa58f02
William <will14lee@gmail.com>
2021-12-16 06:06:00 -0500

    Remove favicon.icon

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,6 +4,8 @@ function Home() {
     return (
         <div>
             <h1>Welcome Home</h1>
+            <h2>Click here if you wish to view created recipients</h2>
+            <h2>Click here if you wish to view created presents</h2>
         </div>
     )
 }
