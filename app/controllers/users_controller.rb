32aedaf5f948751df1891a9efd9c480dd33e6e58
William <will14lee@gmail.com>
2021-12-17 02:21:15 -0500

    Add error handling to users and sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :record_not_found
     skip_before_action :authorize, only: :create
     def create
         user= User.create(user_params)
-        if user.valid?
+        if user.valid?&user.password=user.password_confirmation
             session[:user_id]= user.id
             render json: user, status: :created
         else
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
 
     def show
         user= User.find_by(id: session[:user_id])
-        render json: user
+        render json: user, status: :created
     end
 
     private
@@ -20,4 +21,7 @@ class UsersController < ApplicationController
     def user_params
         params.permit(:username, :password, :password_confirmation, :image_url, :bio)
     end
+
+    def record_not_found
+        return render json: { errors: "Not authorized"}, status: :unauthorized
 end
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 function NavBar() {
     return (
         <div>
-            <p3><Link to="/home">Home</Link></p3>
+            <Link to="/home">Home</Link>
             <br/>
-            <p3><Link to="/recipients/new">Create recipient</Link></p3>
+            <Link to="/recipients/new">Create recipient</Link>
+            {/* <br/> */}
+            {/* <Link to="/items/new">Create items</Link> */}
             <br/>
-            <p3><Link to="/items/new">Create items</Link></p3>
-            <br/>
-            <p3><Link to="/">Logout</Link></p3>
+            <Link to="/">Logout</Link>
         </div>
     )
 }
